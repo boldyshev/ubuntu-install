4e10bf624c7fb2d2215bf445bb74e05681ab5451
boldyshev <boldyshevb@gmail.com>
2024-07-07 20:11:43 +0400

    qbittorrent

--- config_save.sh
+++ config_save.sh
@@ -13,8 +13,8 @@ cp -rp "${wallpapers_path}" "${SCRIPT_DIR}/config"
 dconf dump / > "${SCRIPT_DIR}/config/dconf/$(hostname).dconf"
 
 # Jupyter Lab Desktop App
-mkdir -p "${jupyter_lab_config}"
-cp -rp "${jupyter_lab_config}" "${SCRIPT_DIR}/config"
+#mkdir -p "${jupyter_lab_config}"
+#cp -rp "${jupyter_lab_config}" "${SCRIPT_DIR}/config"
 
 # Input remapper
 cp -rp "${input_remapper_path}/input-remapper-2" "${SCRIPT_DIR}/config"
---
+++ install_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "${0}")
+source "${SCRIPT_DIR}/config/install.conf"
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install packages
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Make docker run without sudo
+usermod -aG docker $user
